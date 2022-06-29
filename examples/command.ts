c655132f6096c4d497df331f8fda82c7fb70613c
Spencer S <spencerschaper@gmail.com>
2022-06-29 18:09:50 -0400

    Delete command.ts

--- examples/command.ts
+++
@@ -1,9 +0,0 @@
-// import { Command } from '../../structures/Command'
-
-// export default new Command({
-//    name: "test",
-//    description: "this is a test command",
-//    permissions: ['ADMINISTRATOR'],
-//    run: async ({ interaction, args client }) => {
-//    }
-//})
