c6e50c09f2e733d4ab490b225afb15524fe77216
Spencer S <spencerschaper@gmail.com>
2022-06-29 18:10:04 -0400

    fixed args system

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,10 +1,10 @@
-import { CommandInteractionOptionResolver } from "discord.js"
 import { client } from ".."
 import { Event } from "../structures/Event"
 import { ExtendedInteraction } from "../typings/Command"
 import embed from '../utils/embed'
 import { codeBlock } from "@discordjs/builders"
 import { errorColor } from '../configuration/embeds.json'
+import { GuildMemberRoleManager, CommandInteractionOptionResolver } from "discord.js"
 
 export default new Event("interactionCreate", async (interaction) => {
     // Chat Input Commands
@@ -14,18 +14,23 @@ export default new Event("interactionCreate", async (interaction) => {
         if (!command) return interaction.followUp("You have used a non existent command");
         if (command.permissions) {
             if (!interaction.memberPermissions?.has(command.permissions)) {
-                let errorEmbed = await embed({ title: 'ERROR!', color: errorColor, description: codeBlock(`You are missing ${command.permissions} permission(s) to run this command!`) })
+                let errorEmbed = await embed({ title: 'ERROR!', color: errorColor, description: codeBlock(`You are missing "${command.permissions}" permission(s) to run this command!`) })
                 return interaction.followUp({ embeds: [errorEmbed] })
             }
         }
-
-        const args = []
-        for (let option of interaction.options.data) {
-            args.push(option.value)
+        if (command.roles) {
+            for (let role of command.roles) {
+                let cacheRole = interaction.guild?.roles.cache.get(role as string)
+                let memberCache = (interaction.member?.roles as GuildMemberRoleManager).cache
+                if (!memberCache.has(cacheRole!.id)) {
+                    let errorEmbed = await embed({ title: 'ERROR!', color: errorColor, description: codeBlock(`You are missing the "${cacheRole?.name.toUpperCase()}" role to run this command!`) })
+                    return interaction.followUp({ embeds: [errorEmbed] })
+                }
+            }
         }
 
         command.run({
-            args: args as string[],
+            args: interaction.options as CommandInteractionOptionResolver,
             client,
             interaction: interaction as ExtendedInteraction
         });
